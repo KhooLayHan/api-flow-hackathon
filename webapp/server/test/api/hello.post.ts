4e6dcf5998dd2c4301782550e14f48ace550558e
KhooLayHan <khoolayhan@github.com>
2025-08-28 13:28:38 +0800

    chore: Updated .gitignore from worker subdirectory

---
+++ webapp/server/test/api/hello.post.ts
@@ -0,0 +1,23 @@
+import { defineEventHandler } from "h3";
+
+// @ts-ignore
+import { workflowQueue } from "~/server/utils/queue";
+
+export default defineEventHandler(async (event) => {
+  console.log("Received request to /api/test/hello");
+
+  const jobName = "test-message";
+  const jobData = {
+    message: `Hello from Nuxt! The time is ${new Date().toLocaleTimeString()}`,
+    sentAt: new Date().toISOString(),
+  };
+
+  // Adding the queue job;
+  await workflowQueue.add(jobName, jobData);
+  console.log(`Job added to queue: ${jobName}, ${jobData.message}`);
+
+  return {
+    status: "ok",
+    message: "Job has been successfully added to the queue.",
+  };
+});
