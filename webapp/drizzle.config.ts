2e6f6358eee718108cbddd680226fcc0eab395f0
KhooLayHan <khoolayhan@github.com>
2025-09-02 17:18:54 +0800

    build(.nuxt): Removed .nuxt and .output files generated from Nuxt, and build the dependencies again.

--- webapp/drizzle.config.ts
+++ webapp/drizzle.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 export default defineConfig({
   dialect: 'postgresql',
   schema: './server/db/schema.ts',
   out: './server/db/migrations',
   // driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
