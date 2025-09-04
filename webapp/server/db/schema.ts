565124966fd4596daa83bd9026c91e3029eb5af9
KhooLayHan <khoolayhan@github.com>
2025-09-04 21:08:30 +0800

    feat(drizzle): Added new 'user_credentials' schema.'

--- webapp/server/db/schema.ts
+++ webapp/server/db/schema.ts
@@ -1,14 +1,12 @@
-// import { pgTable, serial, jsonb, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, text, serial, jsonb, timestamp, varchar, uuid, foreignKey } from 'drizzle-orm/pg-core';
 
-// export const workflows = pgTable('workflows', {
-//   id: serial('id').primaryKey(),
-//   name: varchar('name', { length: 256 }).notNull(),
-//   definition: jsonb('definition').default('{}').notNull(),
-//   createdAt: timestamp('created_at').defaultNow().notNull(),
-//   updatedAt: timestamp('updated_at').defaultNow().notNull(),
-// });
-
-import { pgTable, text, timestamp, uuid, foreignKey } from 'drizzle-orm/pg-core';
+export const workflows = pgTable('workflows', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  definition: jsonb('definition').default('{}').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
 
 export const userCredentials = pgTable('user_credentials', {
   id: uuid('id').primaryKey().defaultRandom(),
