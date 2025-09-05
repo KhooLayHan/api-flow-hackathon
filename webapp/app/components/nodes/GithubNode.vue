dc4818e49b52fcae44a20a74e1f88a71c2585055
KhooLayHan <khoolayhan@github.com>
2025-09-05 16:15:41 +0800

    feat(nodes): Added new GithubNode and SlackNode.

---
+++ webapp/app/components/nodes/GithubNode.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Handle, Position } from '@vue-flow/core';
+</script>
+
+<template>
+  <div class="p-4 border border-gray-300 rounded-md bg-white">
+    <!-- Icon and Title -->
+    <div class="flex items-center space-x-2">
+      <Icon name="mdi:github" class="h-6 w-6" />
+      <div class="font-bold">Github Push Trigger</div>
+    </div>
+    <!-- Output handle -->
+    <Handle type="source" :position="Position.Right" />
+  </div>
+</template>
---
+++ webapp/app/components/nodes/SlackNode.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Handle, Position } from '@vue-flow/core';
+</script>
+
+<template>
+  <div class="p-4 border border-gray-300 rounded-md bg-white">
+    <!-- Icon and Title -->
+    <div class="flex items-center space-x-2">
+      <Icon name="logos:slack-icon" class="h-6 w-6" />
+      <div class="font-bold">Send Slack Message</div>
+    </div>
+    <!-- Output handle -->
+    <Handle type="target" :position="Position.Left" />
+    <Handle type="source" :position="Position.Right" />
+  </div>
+</template>
