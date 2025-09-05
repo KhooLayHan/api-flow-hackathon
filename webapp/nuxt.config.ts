330f18e5c342e215162696d09171b99149978ab5
KhooLayHan <khoolayhan@github.com>
2025-09-05 10:32:51 +0800

    build(supabase): Installed nuxtjs/supabase dependencies.

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -2,5 +2,32 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxtjs/supabase'],
+
+  supabase: {
+    clientOptions: {
+      auth: {
+        flowType: 'pkce', // Recommended for OAuth
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+        // redirectURI: 'http://localhost:3000/auth/callback',
+        // scopes: 'email,profile'
+      },
+    },
+    // Handles the user being redirected to the app after logging in
+    redirectOptions: {
+      login: '/login', // Redirect here if user is not logged in
+      callback: '/confirm', // Redirect here after successful login
+      // error: '/error'
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+      },
+    },
+  },
 });
