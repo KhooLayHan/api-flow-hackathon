825ef1c5b060b2c3f6eb0de8ffb3a16bbad97746
KhooLayHan <khoolayhan@github.com>
2025-09-06 14:49:15 +0800

    Added new column githubRepo to workflows table, and pull the initial schema from the Supabase.

---
+++ webapp/server/db/migrations/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, serial, varchar, jsonb, timestamp, uuid, text } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
+
+
+
+export const workflows = pgTable("workflows", {
+	id: serial().primaryKey().notNull(),
+	name: varchar({ length: 256 }).notNull(),
+	definition: jsonb().default({}).notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow().notNull(),
+	githubRepo: varchar("github_repo", { length: 256 }).notNull(),
+});
+
+export const userCredentials = pgTable("user_credentials", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	userId: uuid("user_id").notNull(),
+	service: text().notNull(),
+	accessToken: text("access_token").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
+});
