980244dce995bac5bbdf910e006564f4d4e8940e
KhooLayHan <khoolayhan@github.com>
2025-09-06 20:09:22 +0800

    fix(redis): Updated the Redis URL environment variable to use the public URL instead.

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxtjs/supabase'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
 
   supabase: {
     clientOptions: {
--- webapp/server/utils/queue.ts
+++ webapp/server/utils/queue.ts
@@ -1,6 +1,8 @@
 import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 
+const redisURL = new URL(process.env.REDIS_URL!);
+
 const connection = new IORedis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
 });
