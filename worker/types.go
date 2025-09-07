31879e44e37ffc45e593f844b26a09f4906a3752
KhooLayHan <khoolayhan@github.com>
2025-09-08 07:20:13 +0800

    feat(types): Updated ExecutedWorkloadPayload, and added GitHubPushPayload.

--- worker/types.go
+++ worker/types.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log/slog"
+
+	"github.com/hibiken/asynq"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 // WorkflowDefinition represents a workflow definition; mirrors the structure of the API.
 type WorkflowDefinition struct {
 	Nodes []Node `json:"nodes"`
@@ -25,4 +32,37 @@ type UserCredential struct {
 	UserID      string
 	Service     string
 	AccessToken string
+}
+
+// TaskProcessor is a struct that holds all the dependencies our task handlers will need.
+type TaskProcessor struct {
+	db     *pgxpool.Pool
+	srv    *asynq.Server
+	logger *slog.Logger
+}
+
+// TestMessagePayload must match the structure of the payload sent by the Nuxt API.
+type TestMessagePayload struct {
+	Message string `json:"message"`
+	SentAt  string `json:"sentAt"`
+}
+
+type ExecuteWorkloadPayload struct {
+	WorkflowID     string            `json:"workflow_id"`
+	UserID         string            `json:"user_id"`
+	TriggerPayload GitHubPushPayload `json:"trigger_payload"`
+}
+
+type GitHubPushPayload struct {
+	Repository struct {
+		FullName string `json:"full_name"`
+	} `json:"repository"`
+	Pusher struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	} `json:"pusher"`
+	HeadCommit struct {
+		Message string `json:"message"`
+		URL     string `json:"url"`
+	} `json:"head_commit"`
 }
